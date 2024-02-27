50bbd702ff4d6c5073476383eee40c8664f27f48
Weyner Ccuiro <wey.090300@gmail.com>
2024-02-27 10:55:17 +0100

    computing energy

---
+++ energy.py
@@ -0,0 +1,25 @@
+from scipy.integrate import dblquad
+import numpy as np
+
+import csv
+
+def energy(d, u, j, a):
+  inte = dblquad(lambda x, y: np.power(2*np.pi,-2)*np.sqrt(4*d**2*(u-j)**2 + a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
+  en = -inte[0] - 2*(j-u)*d**2 + 0.5*u*a**2
+  return en
+
+def sum_values_and_write_to_file(input_file, output_file):
+  with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
+    for row in reader:
+      row = np.array(row, dtype=float)
+      ene = energy(row[2], row[0], 0.1, row[1])
+      # print(ene)
+      # row_sum = sum(map(int, row))
+      writer.writerow([row[0],ene])
+
+input_file = './dataJ=0.1_U=0.01-18_100.dat'  # Replace 'input.csv' with the path to your input file
+output_file = './dataJ=0.1_U=0.01-18_100_energy.dat'  # Replace 'output.csv' with the path to your output file
+sum_values_and_write_to_file(input_file, output_file)
+# print("Sum of values in each row has been written to", output_file)
--- main3par.py
+++ main3par.py
@@ -3,11 +3,66 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+import random as rand
+
+def IntDelta(d, u, j, a, start, N, var):
+  """ d: BCS parameter, u = U/t, j = J/t, a = CDW parameter"""
+  acc = 0
+  
+  x1 = 0
+  y1 = 0
+    
+  delta = lambda x,y: (2/(2*np.pi)**2)*d*(u-j)/np.sqrt(16*(np.cos(x) + np.cos(y))**2 + 4*(d**2)*(u-j)**2 + (a**2)*u**2)
+  for i in range(N):
+    rand1 = rand.random()
+    rand2 = rand.random()
+    rand3 = rand.random()
+    x_try = ((x1 + var*(1 - 2*rand1))- np.pi)%(2*np.pi) + np.pi
+    y_try = ((y1 + var*(1 - 2*rand2))- np.pi)%(2*np.pi) + np.pi
+    z1 = delta(x_try,y_try)/delta(x1,y1)
+    valInt = 0
+    if(rand3 < z1):
+      x1 = x_try
+      y1 = y_try
+      valInt += delta(x1,y1)
+      acc +=1
+    else:
+      valInt += delta(x1,y1) 
+  valInt = valInt/N
+  return valInt, acc/N
+    
+
+
+def IntAlpha(d, u, j, a, start, N, var):
+  """ d: BCS parameter, u = U/t, j = J/t, a = CDW parameter"""
+  acc = 0
+  x1 = 0
+  y1 = 0
+  alpha =   lambda x,y: (2/(2*np.pi)**2)*a*(u)/np.sqrt(16*(np.cos(x) + np.cos(y))**2 + 4*(d**2)*(u-j)**2 + (a**2)*u**2)
+  for i in range(N):
+    rand1 = rand.random()
+    rand2 = rand.random()
+    rand3 = rand.random()
+    x_try = ((x1 + var*(1 - 2*rand1))- np.pi)%(2*np.pi) + np.pi
+    y_try = ((y1 + var*(1 - 2*rand2))- np.pi)%(2*np.pi) + np.pi
+    z1 = alpha(x_try,y_try)/alpha(x1,y1)
+    valInt = 0
+    if(rand3 < z1):
+      x1 = x_try
+      y1 = y_try
+      valInt += alpha(x1,y1)
+      acc +=1
+    else:
+      valInt += alpha(x1,y1) 
+  valInt = valInt/N
+  return valInt, acc/N       
+
+
 def Delta(d, u, j, a):
-  return dblquad(lambda x, y: np.power(2*np.pi,-2)*d*(u-j)/np.sqrt(4*d**2*(u-j)**2 + a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
+  return dblquad(lambda x, y: 2*np.power(2*np.pi,-2)*d*(u-j)/np.sqrt(4*d**2*(u-j)**2 + a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
 
 def alpha(d, u, j, a):
-  return dblquad(lambda x, y: np.power(2*np.pi,-2)*d*u/np.sqrt(4*d**2*(u-j)**2 + a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
+  return dblquad(lambda x, y: 2*np.power(2*np.pi,-2)*a*u/np.sqrt(4*d**2*(u-j)**2 + a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
 
 def recursive(d, u, j, a):
   d1 = Delta(d, u, j, a)[0]
@@ -18,14 +73,15 @@ def recursive(d, u, j, a):
     return d1, a1
   return recursive(d1, u, j, a1)
 
-u_arr = np.linspace(0.01, 15, 60)
+u_arr = np.linspace(0.01, 20, 200)
 d = []
 c = 1
-with open('data3PnJ0p5.dat', 'w') as file: 
+with open('dataJ=0.1_U=0.01-18_100_delta=0.dat', 'w') as file: 
   for i in u_arr:
-    d, a = recursive(i+1, i, 0.1, i+1)
+    d, a = recursive(0, i, 0.1, i+1)
     file.write(str(i)+","+str(a)+","+str(d)+"\n")
     print(c,"/",len(u_arr), d-Delta(d, i, 0.1, a)[0], a-alpha(d, i, 0.1, a)[0])
+    # print(d-IntDelta(d, i, 0.1, a, rand.random(), 300, 0.5)[0], a-IntAlpha(d, i, 0.1, a, rand.random(), 300, 0.5)[0] )
     c+=1
 
 # plt.plot(u_arr,d)
