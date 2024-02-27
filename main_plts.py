18156d47d36ccb251be5b256c68fafe13b41f90a
edozavatti <edoardo.zavatti@gmail.com>
2024-02-27 11:10:08 +0100

    .

--- main_plts.py
+++ main_plts.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+######## J=0, SC vs. CDW ########
+
 i3, data3 = np.loadtxt("data_cdw.dat", delimiter=',', unpack=True)
 i2, data2 = np.loadtxt("data2.dat", delimiter=',', unpack=True)
 
@@ -12,3 +14,6 @@ plt.title("Non interacting case: comparison between SC and CDW")
 plt.xlabel('U/t')
 plt.ylabel(r'$\Delta$')
 plt.show()
+
+######## J ########
+
