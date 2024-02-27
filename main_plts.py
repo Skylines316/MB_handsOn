7d40a26e1eb93f1f81ec9215063da291116a60b3
edozavatti <edoardo.zavatti@gmail.com>
2024-02-27 11:06:08 +0100

    main_plts for plotting

---
+++ main_plts.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+i3, data3 = np.loadtxt("data_cdw.dat", delimiter=',', unpack=True)
+i2, data2 = np.loadtxt("data2.dat", delimiter=',', unpack=True)
+
+plt.plot(i3,data3, label='CDW')
+plt.plot(i2,2*data2, label='SC')
+plt.legend()
+plt.grid(True)
+plt.title("Non interacting case: comparison between SC and CDW")
+plt.xlabel('U/t')
+plt.ylabel(r'$\Delta$')
+plt.show()
