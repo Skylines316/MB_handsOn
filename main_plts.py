a8d3dc42dadcc7012ba7f9d81760f0ecee022724
edozavatti <edoardo.zavatti@gmail.com>
2024-02-28 14:25:13 +0100

    .

--- main_plts.py
+++ main_plts.py
@@ -46,21 +46,18 @@ J_values = [0.1,0.3,0.5,0.7,1]
 fig, axs = plt.subplots(1, len(J_values), figsize=(15, 5))
 
 for i in range(len(J_values)):
-    g1, e1 = np.loadtxt(f"Data/energy/dataJ={J_values[i]}_U=0.01-20_200_alpha=0_energy.dat", delimiter=',', unpack=True)
-    g2, e2 = np.loadtxt(f"Data/energy/dataJ={J_values[i]}_U=0.01-20_200_delta=0_energy.dat", delimiter=',', unpack=True)
-    g3, e3 = np.loadtxt(f"Data/energy/dataJ={J_values[i]}_U=0.01-20_200_energy.dat", delimiter=',', unpack=True)
+    g1, e1 = np.loadtxt(f"Data/energy/J={J_values[i]}/dataJ={J_values[i]}_U=0.01-20_200_alpha=0_energy.dat", delimiter=',', unpack=True)
+    g2, e2 = np.loadtxt(f"Data/energy/J={J_values[i]}/dataJ={J_values[i]}_U=0.01-20_200_delta=0_energy.dat", delimiter=',', unpack=True)
+    g3, e3 = np.loadtxt(f"Data/energy/J={J_values[i]}/dataJ={J_values[i]}_U=0.01-20_200_energy.dat", delimiter=',', unpack=True)
 
-    axs[i].plot(g1,e1, label='Only SC', marker='.',s=8)
-    axs[i].plot(g2,e2, label='Only CDW', marker='*',s=8)
-    axs[i].plot(g3,e3, label='SC + CDW', marker='v',s=8)
-    axs[i].set_title(f'J={J_values[i]}')
-
-cbar = fig.colorbar(scatter, ax=axs[-1])
-cbar.set_label('U/t')
+    axs[i].plot(g1,e1, label='Only SC', linestyle='-', marker='o', markersize=2)
+    axs[i].plot(g2,e2, label='Only CDW', linestyle='-', marker='v', markersize=2)
+    axs[i].plot(g3,e3, label='SC + CDW', linestyle='-', marker='*', markersize=2)
+    axs[i].set_title(f'Energy, J={J_values[i]}')
 
 for ax in axs:
-    ax.set_xlabel(r'$\Delta_{CDW}$')
-    ax.set_ylabel(r'$\Delta_{SC}$')
+    ax.set_xlabel(r'U/t$')
+    ax.set_ylabel(r'Energy$')
     ax.grid(True)
     ax.legend()
 
