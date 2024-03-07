ab1044be505931ef84b548cdeadbb18955313219
Weyner Ccuiro <wey.090300@gmail.com>
2024-03-07 18:30:46 +0100

    data considering minimization

--- energy.py
+++ energy.py
@@ -8,18 +8,25 @@ def energy(d, u, j, a):
   en = -inte[0] - 2*(j-u)*d**2 + 0.5*u*a**2
   return en
 
+def relation(d, u, j, a):
+  ratioa = (2*np.pi)**2*(-a*u)-142.957/(a*u)+u*a**2/2
+  ratiod = (2*np.pi)**2*(-2*d*(j-u))-71.4784/(d*(j-u))-2*(j-u)*d**2
+  return ratioa, ratiod
+
 def sum_values_and_write_to_file(input_file, output_file):
   with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
     reader = csv.reader(infile)
     writer = csv.writer(outfile)
+    J = 3
     for row in reader:
       row = np.array(row, dtype=float)
-      ene = energy(row[2], row[0], 0.1, row[1])
+      ra,rd = relation(row[2], row[0], J, row[1])
       # print(ene)
       # row_sum = sum(map(int, row))
-      writer.writerow([row[0],ene])
+      # writer.writerow([row[0],r])
+      print(row[0],ra,rd)
 
-input_file = './Data/J=0/dataJ=0_U=0.01-20_200_alpha=0.dat'  # Replace 'input.csv' with the path to your input file
-output_file = './Data/energy/J=0/dataJ=0_U=0.01-20_200_alpha=0_energy.dat'  # Replace 'output.csv' with the path to your output file
+input_file = './dataJ=3_U=0.01-10_400_w_energies.dat'  # Replace 'input.csv' with the path to your input file
+output_file = './dataJ=3_U=0.01-10_400_w_energies_ratios.dat'  # Replace 'output.csv' with the path to your output file
 sum_values_and_write_to_file(input_file, output_file)
 # print("Sum of values in each row has been written to", output_file)
--- main3par.py
+++ main3par.py
@@ -62,10 +62,10 @@ def Delta(d, u, j, a):
   return dblquad(lambda x, y: 2*np.power(2*np.pi,-2)*d*(u-j)/np.sqrt(4*d**2*(u-j)**2 + a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
 
 def Delta_zero(d, u, j):
-  return dblquad(lambda x, y: 2*np.power(2*np.pi,-2)*d*np.abs(u-j)/np.sqrt(4*d**2*(u-j)**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
+  return dblquad(lambda x, y: 1*np.power(2*np.pi,-2)*d*np.abs(u-j)/np.sqrt(4*d**2*(u-j)**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
 
 def alpha_zero(u, a):
-  return dblquad(lambda x, y: 2*np.power(2*np.pi,-2)*a*u/np.sqrt(a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
+  return dblquad(lambda x, y: 1*np.power(2*np.pi,-2)*a*u/np.sqrt(a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
 
 def alpha(d, u, j, a):
   return dblquad(lambda x, y: 2*np.power(2*np.pi,-2)*a*u/np.sqrt(4*d**2*(u-j)**2 + a**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)
@@ -82,32 +82,34 @@ def recursive(d, u, j, a):
   # print(d1[1])
   # print(difd, d1[0])
   if abs(difd) <= 1e-2 and abs(difa) <= 1e-2:
-    en_Delta = -dblquad(lambda x, y: np.power(2*np.pi,-2)*np.sqrt(4*d1**2*(u-j)**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)[0] - 2*(j-u)*d**2 + 0.5*u*a**2
+    en_Delta = -dblquad(lambda x, y: np.power(2*np.pi,-2)*np.sqrt(4*d1**2*(u-j)**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)[0] - 2*(j-u)*d1**2
     
-    en_alpha = -dblquad(lambda x, y: np.power(2*np.pi,-2)*np.sqrt( a1**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)[0] + 0.5*u*a**2
+    en_alpha = -dblquad(lambda x, y: np.power(2*np.pi,-2)*np.sqrt( a1**2*u**2 + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)[0] + 0.5*u*a1**2
 
-    en_base = -dblquad(lambda x, y: np.power(2*np.pi,-2)*np.sqrt( + 16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)[0]
+    en_base = -dblquad(lambda x, y: np.power(2*np.pi,-2)*np.sqrt(16*(np.cos(x)+np.cos(y))**2), -np.pi, np.pi, lambda x: -np.pi, lambda x: np.pi)[0]
 
     if en_Delta > en_alpha:
-      # if en_alpha > en_base:
-      #   return 0, 0
-      # else:
-      return 0, a1
+      if en_alpha > en_base:
+        return 0, 0, en_Delta, en_alpha, en_base
+      else:
+        return 0, a1, en_Delta, en_alpha, en_base
     else:
-      # if en_Delta > en_base:
-      #   return 0, 0
-      # else:
-      return d1, 0      
+      if en_Delta > en_base:
+        return 0, 0, en_Delta, en_alpha, en_base
+      else:
+        return d1, 0, en_Delta, en_alpha, en_base     
   return recursive(d1, u, j, a1)
 
-u_arr = np.linspace(0.01, 20, 200)
+u_arr = np.linspace(0.01, 10, 400)
 d = []
 c = 1
-with open('dataJ=5_U=0.01-20_200_nobase.dat', 'w') as file: 
+J = 9
+filename = 'dataJ='+str(J)+'_U=0.01-10_400_w_energies.dat'
+with open(filename, 'w') as file: 
   for i in u_arr:
-    d, a = recursive(i+1, i, 5, i+1)
-    file.write(str(i)+","+str(a)+","+str(d)+"\n")
-    print(c,"/",len(u_arr), round(a-alpha_zero(i, a)[0],4), round(d-Delta_zero(d, i, 5)[0],4), "U="+str(i), "delta="+str(round(d,4)), "alpha="+str(round(a,4)))
+    d, a , eD, eA, eB= recursive(i+1, i, J, i+1)
+    file.write(str(i)+","+str(a)+","+str(d)+","+str(eA)+","+str(eD)+","+str(eB)+"\n")
+    print(c,"/",len(u_arr), round(a-alpha_zero(i, a)[0],4), round(d-Delta_zero(d, i, J)[0],4), "U="+str(i), "delta="+str(round(d,4)), "alpha="+str(round(a,4)))
     # print(d-IntDelta(d, i, 0.1, a, rand.random(), 300, 0.5)[0], a-IntAlpha(d, i, 0.1, a, rand.random(), 300, 0.5)[0] )
     c+=1
 
